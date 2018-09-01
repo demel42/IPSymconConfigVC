14dd2849775d7155dcd47f917b62a1e712fe85e3
demel42 <demel42@web.de>
2018-09-01 10:33:41 +0200

    first commit

---
+++ ConfigVC/module.php
@@ -0,0 +1,88 @@
+<?php
+
+require_once __DIR__ . '/../libs/common.php';  // globale Funktionen
+
+if (@constant('IPS_BASE') == null) {
+    // --- BASE MESSAGE
+    define('IPS_BASE', 10000);							// Base Message
+    define('IPS_KERNELSHUTDOWN', IPS_BASE + 1);			// Pre Shutdown Message, Runlevel UNINIT Follows
+    define('IPS_KERNELSTARTED', IPS_BASE + 2);			// Post Ready Message
+    // --- KERNEL
+    define('IPS_KERNELMESSAGE', IPS_BASE + 100);		// Kernel Message
+    define('KR_CREATE', IPS_KERNELMESSAGE + 1);			// Kernel is beeing created
+    define('KR_INIT', IPS_KERNELMESSAGE + 2);			// Kernel Components are beeing initialised, Modules loaded, Settings read
+    define('KR_READY', IPS_KERNELMESSAGE + 3);			// Kernel is ready and running
+    define('KR_UNINIT', IPS_KERNELMESSAGE + 4);			// Got Shutdown Message, unloading all stuff
+    define('KR_SHUTDOWN', IPS_KERNELMESSAGE + 5);		// Uninit Complete, Destroying Kernel Inteface
+    // --- KERNEL LOGMESSAGE
+    define('IPS_LOGMESSAGE', IPS_BASE + 200);			// Logmessage Message
+    define('KL_MESSAGE', IPS_LOGMESSAGE + 1);			// Normal Message
+    define('KL_SUCCESS', IPS_LOGMESSAGE + 2);			// Success Message
+    define('KL_NOTIFY', IPS_LOGMESSAGE + 3);			// Notiy about Changes
+    define('KL_WARNING', IPS_LOGMESSAGE + 4);			// Warnings
+    define('KL_ERROR', IPS_LOGMESSAGE + 5);				// Error Message
+    define('KL_DEBUG', IPS_LOGMESSAGE + 6);				// Debug Informations + Script Results
+    define('KL_CUSTOM', IPS_LOGMESSAGE + 7);			// User Message
+}
+
+if (!defined('vtBoolean')) {
+	define('vtBoolean', 0);
+	define('vtInteger', 1);
+	define('vtFloat', 2);
+	define('vtString', 3);
+	define('vtArray', 8);
+	define('vtObject', 9);
+}
+
+class ConfigVC extends IPSModule
+{
+    use ConfigVCCommon;
+
+    public function Create()
+    {
+        parent::Create();
+
+        $this->RegisterPropertyString('url', '');
+    }
+
+    public function ApplyChanges()
+    {
+        parent::ApplyChanges();
+
+        $vpos = 0;
+        $this->MaintainVariable('Summary', $this->Translate('Summary of last Update'), vtString, '', $vpos++, true);
+        $this->MaintainVariable('LastUpdate', $this->Translate('Last versioning'), vtInteger, '~UnixTimestamp', $vpos++, true);
+
+        $this->SetStatus(102);
+
+        $this->SetUpdateInterval();
+    }
+
+    public function GetConfigurationForm()
+    {
+        $formElements = [];
+        $formElements[] = ['type' => 'ValidationTextBox', 'name' => 'url', 'caption' => 'Git-Repository'];
+
+        $formActions = [];
+        $formActions[] = ['type' => 'Label', 'label' => 'Action takes up to 1 minute (depending on amount of data)'];
+        $formActions[] = ['type' => 'Button', 'label' => 'Perform', 'onClick' => 'ConfigVC_Perform($id);'];
+		$formActions[] = ['type' => 'Label', 'label' => '____________________________________________________________________________________________________'];
+		$formActions[] = [
+							'type'    => 'Button',
+							'caption' => 'Module description',
+							'onClick' => 'echo "https://github.com/demel42/IPSymconConfigVC/blob/master/README.md";'
+						];
+
+        $formStatus = [];
+        $formStatus[] = ['code' => '101', 'icon' => 'inactive', 'caption' => 'Instance getting created'];
+        $formStatus[] = ['code' => '102', 'icon' => 'active', 'caption' => 'Instance is active'];
+        $formStatus[] = ['code' => '104', 'icon' => 'inactive', 'caption' => 'Instance is inactive'];
+		$formStatus[] = ['code' => '201', 'icon' => 'inactive (invalid configuration)', 'caption' => 'Instance is inactive (invalid configuration)'];
+
+        return json_encode(['elements' => $formElements, 'actions' => $formActions, 'status' => $formStatus]);
+    }
+
+    public function Perform()
+    {
+    }
+}
---
+++ README.md
@@ -0,0 +1,70 @@
+# IPSymconConfigVC
+
+[![IPS-Version](https://img.shields.io/badge/Symcon_Version-5.0-red.svg)](https://www.symcon.de/service/dokumentation/entwicklerbereich/sdk-tools/sdk-php/)
+![Module-Version](https://img.shields.io/badge/Modul_Version-1.0-blue.svg)
+![Code](https://img.shields.io/badge/Code-PHP-blue.svg)
+[![License](https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-green.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0/)
+[![StyleCI](https://github.styleci.io/repos/126683101/shield?branch=master)](https://github.styleci.io/repos/xxxxxxxxx)
+
+## Dokumentation
+
+**Inhaltsverzeichnis**
+
+1. [Funktionsumfang](#1-funktionsumfang)
+2. [Voraussetzungen](#2-voraussetzungen)
+3. [Installation](#3-installation)
+4. [Funktionsreferenz](#4-funktionsreferenz)
+5. [Konfiguration](#5-konfiguration)
+6. [Anhang](#6-anhang)
+7. [Versions-Historie](#7-versions-historie)
+
+## 1. Funktionsumfang
+
+## 2. Voraussetzungen
+
+ - IP-Symcon ab Version 5
+
+## 3. Installation
+
+Die Konsole von IP-Symcon öffnen. Im Objektbaum unter Kerninstanzen die Instanz __*Modules*__ durch einen doppelten Mausklick öffnen.
+
+In der _Modules_ Instanz rechts oben auf den Button __*Hinzufügen*__ drücken.
+
+In dem sich öffnenden Fenster folgende URL hinzufügen:
+
+`https://github.com/demel42/IPSymconConfigVC.git`
+
+und mit _OK_ bestätigen.
+
+Anschließend erscheint ein Eintrag für das Modul in der Liste der Instanz _Modules_
+
+### c. Einrichtung in IPS
+
+In IP-Symcon nun _Instanz hinzufügen_ (_CTRL+1_) auswählen unter der Kategorie, unter der man die Instanz hinzufügen will, und Hersteller _(sonstiges)_ und als Gerät _ConfigVC_ auswählen.
+
+## 4. Funktionsreferenz
+
+### zentrale Funktion
+
+`boolean ConfigVC_XXXX(integer $InstanzID)`<br>
+
+## 5. Konfiguration:
+
+### Variablen
+
+| Eigenschaft                     | Typ      | Standardwert | Beschreibung |
+| :-----------------------------: | :-----:  | :----------: | :----------------------------------------------------------------------------------------------------------: |
+| Aktualisiere Daten ...          | integer  | 60           | Aktualisierungsintervall, Angabe in Minuten |
+
+## 6. Anhang
+
+GUIDs
+
+- Modul: `{EA906449-5DDA-4D99-9110-276BB31B6FFC}`
+- Instanzen:
+  - ConfigVC: `{396EA137-2E5F-413A-A996-D662158EA481}`
+
+## 7. Versions-Historie
+
+- 1.0 @ 01.09.2018<br>
+  Initiale Version
