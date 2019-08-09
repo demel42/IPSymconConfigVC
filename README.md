4df911ee2f2caccf235fd8b8dee3d894197e6c99
IP-Symcon <root@ips-prod>
2019-08-09 14:47:15 +0200

    Schreibfehler korrigiert

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # IPSymconConfigVC
 
 [![IPS-Version](https://img.shields.io/badge/Symcon_Version-5.0+-red.svg)](https://www.symcon.de/service/dokumentation/entwicklerbereich/sdk-tools/sdk-php/)
-![Module-Version](https://img.shields.io/badge/Modul_Version-1.16-blue.svg)
+![Module-Version](https://img.shields.io/badge/Modul_Version-1.17-blue.svg)
 ![Code](https://img.shields.io/badge/Code-PHP-blue.svg)
 [![License](https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-green.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0/)
 [![StyleCI](https://github.styleci.io/repos/146979798/shield?branch=master)](https://github.styleci.io/repos/146979798)
@@ -282,6 +282,9 @@ GUIDs
   - ConfigVC: `{396EA137-2E5F-413A-A996-D662158EA481}`
 
 ## 7. Versions-Historie
+
+- 1.17 @ 09.08.2019 14:32<br>
+  - Schreibfehler korrigiert
 
 - 1.16 @ 16.07.2019 18:14<br>
   - es war möglich trotz fehlerhafter Konfiguration die Test-Funktionen aufzurufen; das gibt nun eine entsprechende Meldung.
--- libs/common.php
+++ libs/common.php
@@ -50,7 +50,7 @@ trait ConfigVCCommon
         return $ret;
     }
 
-    private function CreateVarProfile($Name, $ProfileType, $Suffix, $MinValue, $MaxValue, $StepSize, $Digits, $Icon, $Asscociations = '')
+    private function CreateVarProfile($Name, $ProfileType, $Suffix, $MinValue, $MaxValue, $StepSize, $Digits, $Icon, $Associations = '')
     {
         if (!IPS_VariableProfileExists($Name)) {
             IPS_CreateVariableProfile($Name, $ProfileType);
@@ -58,8 +58,8 @@ trait ConfigVCCommon
             IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);
             IPS_SetVariableProfileDigits($Name, $Digits);
             IPS_SetVariableProfileIcon($Name, $Icon);
-            if ($Asscociations != '') {
-                foreach ($Asscociations as $a) {
+            if ($Associations != '') {
+                foreach ($Associations as $a) {
                     $w = isset($a['Wert']) ? $a['Wert'] : '';
                     $n = isset($a['Name']) ? $a['Name'] : '';
                     $i = isset($a['Icon']) ? $a['Icon'] : '';
